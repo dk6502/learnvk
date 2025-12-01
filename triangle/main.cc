fda154384f107357ba572c0e22fb1e68aeed9b5e
dk6502 <dk6502@penis.com>
2025-12-01 09:20:33 -0600

    init

---
+++ triangle/main.cc
@@ -0,0 +1,53 @@
+#include <GLFW/glfw3.h>
+#include <print>
+#include <vulkan/vulkan_raii.hpp>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  GLFWwindow *window = nullptr;
+  void initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+  void initVulkan() {}
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+  }
+};
+
+int main() {
+  HelloTriangleApplication app;
+
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::println("{}", e.what());
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
